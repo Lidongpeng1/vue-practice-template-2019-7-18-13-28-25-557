3fbe82e3877eaa1bf093f6bba0e646375c77acde
“your_uLiDongpeng1 <l>
2019-07-22 23:13:55 +0800

    creat showmethod in methods

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="item-list">
         <ol>
-            <li v-for="(item, index) in items" :key="index">
+            <li v-for="(item, index) in items" :key="item.content + index" v-show="showMethod(item)">
                 <input type="checkbox" v-model="item.isChecked" />
                 <span v-bind:class="{checked: item.isChecked}">{{  item.content  }}</span>
             </li>
@@ -12,7 +12,17 @@
 <script>
     export default {
         name: "ItemList",
-        props: ['items']
+        computed: {
+            items() {
+                return this.$store.state.items;
+            }
+        },
+        methods: {
+            showMethod(item) {      //未实现：用每个item自己的isChecked属性判断
+                return this.$store.state.isShowAll ? true :
+                    (this.$store.state.isShowActive ? !item.isChecked : item.isChecked);
+            }
+        }
     }
 </script>
 
