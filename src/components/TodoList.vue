8152eadafcf2fd12ffba7c3f449666c2a143163e
“your_uLiDongpeng1 <l>
2019-07-22 23:18:02 +0800

    refactor todolist,make it simple

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,57 +1,25 @@
 <template>
     <div class="container">
         <Header/>
-        <div>
-            <input class="input-text" type="text" v-model="newItem">
-            <div id="button" @click="add">Add</div>
-        </div>
-        <ItemList :items="showItems" />
-        <MyFilter v-on:status="filter" />
+        <InputBox/>
+        <ItemList/>
+        <MyFilter/>
     </div>
 </template>
 
 <script>
-    import Header from "./Header";
-    import ItemList from "./ItemList"
-    import MyFilter from "./MyFilter"
+    import Header from "@/components/Header";
+    import InputBox from "@/components/InputBox";
+    import ItemList from "@/components/ItemList";
+    import MyFilter from "@/components/MyFilter";
+
     export default {
         name: "TodoList",
         components: {
-            Header,
+            MyFilter,
             ItemList,
-            MyFilter
-        },
-        data() {
-            return {
-                newItem: "",
-                items: [],
-                showItems: [],
-            }
-        },
-        methods: {
-            add() {
-                this.items.push({
-                    isChecked: false,
-                    content: this.newItem
-                });
-                this.newItem = "";
-                this.showItems = this.items.slice();
-            },
-            filter(status) {
-                const active = 1;
-                const complete = 2;
-                switch(status) {
-                    case active:
-                        this.showItems = this.items.filter(item => item.isChecked == false);
-                        break;
-                    case complete:
-                        this.showItems = this.items.filter(item => item.isChecked == true);
-                        break;
-                    default:
-                        this.showItems = this.items.slice();
-                        break;
-                }
-            }
+            InputBox,
+            Header
         }
     }
 </script>
