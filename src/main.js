1da6c6000adb6f77ef4cd4de12165963463b404d
“your_uLiDongpeng1 <l>
2019-07-22 23:09:28 +0800

    creat new Vuex.Store for store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        items: [],
+        isShowAll: true,
+        isShowActive: false
+    },
+    mutations: {
+        push(state, item) {
+            state.items.push(item);
+        },
+        setIsShowAll(state, isShowAll) {
+            state.isShowAll = isShowAll;
+        },
+        setIsShowActive(state, isShowActive) {
+            state.isShowAll = false;
+            state.isShowActive = isShowActive;
+        }
+    }
+})
 
 Vue.config.productionTip = false
 
-import store from './store/store'
 new Vue({
-  store,
-  render: h => h(App),
+    store,
+    render: h => h(App),
 }).$mount('#app')
