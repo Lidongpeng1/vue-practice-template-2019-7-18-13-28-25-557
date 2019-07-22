46c7b6e2906383b6647f911ce72d7755c2866ac3
“your_uLiDongpeng1 <l>
2019-07-22 23:11:17 +0800

    refactor inputbox component

---
+++ src/components/InputBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input class="input-text" type="text" v-model="input">
+        <div id="button" @click="add">Add</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputBox",
+        data() {
+            return {
+                input: "",
+            }
+        },
+        methods: {
+            add() {
+                if (this.input == undefined) {  //未实现
+                    alert("输入为空")
+                } else {
+                    this.$store.commit('push',{
+                        isChecked: false,
+                        content: this.input
+                    });
+                    this.input = "";
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
