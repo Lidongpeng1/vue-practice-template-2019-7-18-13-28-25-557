7215fd40bbb529ae48a15ca36835d84643857507
“your_uLiDongpeng1 <l>
2019-07-23 22:02:03 +0800

    updata main.js,and import storefile in it,include index.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex);
-
-const store = new Vuex.Store({
-    state: {
-        items: [],
-        isShowAll: true,
-        isShowActive: false
-    },
-    mutations: {
-        push(state, item) {
-            state.items.push(item);
-        },
-        setIsShowAll(state, isShowAll) {
-            state.isShowAll = isShowAll;
-        },
-        setIsShowActive(state, isShowActive) {
-            state.isShowAll = false;
-            state.isShowActive = isShowActive;
-        }
-    }
-})
-
-Vue.config.productionTip = false
+import store from './store/index'
 
 new Vue({
     store,
